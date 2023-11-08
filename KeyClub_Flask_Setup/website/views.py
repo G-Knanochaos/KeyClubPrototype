52b9f4d635fd91f51a88f33ff07efdd54ea2e489
G-K nanochaos <56146097+nanochaos@users.noreply.github.com>
2023-11-07 20:44:31 -0800

    Proxy Fix and Fetch merge

--- KeyClub_Flask_Setup/website/views.py
+++ KeyClub_Flask_Setup/website/views.py
@@ -4,25 +4,35 @@ import json
 from datetime import date
 import os
 
-def fetch_events(s=1,n=4): #group multiplier, group size
-    url = url_for("static", filename = "json/events.json")
+requests_map = {
+    "events":"https://script.google.com/macros/s/AKfycbzQFrs5AnxX-JjJuBOt_B41QoHLyc2iUmREIcJzIql6nzWW7VOxiTMNJchRK6Ptltjwlg/exec",
+    "images":"https://script.google.com/macros/s/AKfycbzdLT7ZYBBf50gHiWll_qUatXHToagXeSqRsra7U1gitvEkDpgsUVXm-dr311D6BYzZ/exec"
+}
+def fetch(type="events",n=4,s=1):
+    print(f"Fetching {type}!")
+    url = url_for("static", filename = f"json/{type}.json")
+    add = False
     with open(os.path.join(os.path.dirname(__file__),url[1:]), "r") as file:
         f = json.load(file)
     if f.get("date",None) == date.today():
-        return f
+        if len(f[type]) == n:
+            return f[type]
+        add = True
+    n = len(f[type])-n
     payload = {"s":str(s),"n":str(n)}
-    r = requests.get("https://script.google.com/macros/s/AKfycbzQFrs5AnxX-JjJuBOt_B41QoHLyc2iUmREIcJzIql6nzWW7VOxiTMNJchRK6Ptltjwlg/exec", params = payload).text
+
+
+    r = requests.get(requests_map[type], params = {}).text #wtf man
+
+    print(r)
     j = json.loads(r)
     j["date"] = date.today().strftime('%m/%d/%Y')
+    print(j[type])
+    result = (f[type] if add else [])+j[type]
     with open(os.path.join(os.path.dirname(__file__),url[1:]), "w") as file:
         file.write(json.dumps(j))
-    return j["events"]
+    return result
 
-def fetch_images(s=True,n=4): #stratify, number of images
-    payload = {"s":str(s),"n":str(n)}
-    r = requests.get("https://script.google.com/macros/s/AKfycbz8DFStA8H6c-VMfsryOgBksfUNSbLD7qr7_vPlq33Zp9QWrgKeXSOFQhUyQwuvnBlk/exec", params = payload).text
-    j = json.loads(r)
-    return j["links"]
 
 views = Blueprint('views', __name__)
 
@@ -33,8 +43,10 @@ def base():
 
 @views.route('')
 def default():
-    events = fetch_events()
-    return render_template("index.html", events=events)
+    events = fetch("events")
+    images = fetch("images")
+    print(images)
+    return render_template("index.html", images=images, events=events)
 
 @views.route('landing')
 def landing():
