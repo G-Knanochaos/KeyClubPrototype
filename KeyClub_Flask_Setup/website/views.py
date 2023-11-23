596999e2e4003380013827a36eddc0092d034dc6
G-K nanochaos <56146097+nanochaos@users.noreply.github.com>
2023-11-23 13:42:17 -0800

    Misc page full integration

--- KeyClub_Flask_Setup/website/views.py
+++ KeyClub_Flask_Setup/website/views.py
@@ -9,12 +9,13 @@ requests_map = {
     "events":"https://script.google.com/macros/s/AKfycbzQFrs5AnxX-JjJuBOt_B41QoHLyc2iUmREIcJzIql6nzWW7VOxiTMNJchRK6Ptltjwlg/exec",
     "images":"https://script.google.com/macros/s/AKfycbzdLT7ZYBBf50gHiWll_qUatXHToagXeSqRsra7U1gitvEkDpgsUVXm-dr311D6BYzZ/exec",
     "FAQs":"https://script.google.com/macros/s/AKfycbwOy_dZXH1prFKOj_ZU-mgU9Vodot-KYK5FSpA0uPX8RyKlSNN6Lls8GZshkNGEoWoi6Q/exec",
+    "Misc":"https://script.google.com/macros/s/AKfycbzmioExdkHHboQDNtZUjMjAlgcKrYutKkjZrJv3DpT0q-ruQI3tzo-wtChISbxLAnM/exec"
 }
-def fetch(type="events",n=4,s=1,override=False):
+def fetch(type="events",n=4,s=1,override=False):#if override is set to True, changes won't be fetched until next day and changes will completely overwrite past state
     print(f"Fetching {type}!")
     url = url_for("static", filename = f"json/{type}.json")
     today = date.today().strftime("%m%d%y")
-    with open(os.path.join(os.path.dirname(__file__),url[1:]), "r") as file:
+    with open(os.path.join(os.path.dirname(__file__),url[1:]), "a+") as file:
         try: 
             f = json.load(file)
         except:
@@ -83,12 +84,13 @@ def past_events():
 @views.route("resources-faq.html")
 def faq():
     faqs = fetch("FAQs",override=True)
-    print(faqs)
     return render_template("resources-faq.html", faqs=faqs)
 
 @views.route("resources-misc.html")
 def misc():
-    return render_template("resources-misc.html")
+    misc = fetch("Misc",override=True)
+    print(misc)
+    return render_template("resources-misc.html",misc=misc)
 
 @views.route("resources-newsletters.html")
 def newsletters():
