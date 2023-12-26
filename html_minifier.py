8b9b2ca1d89551a99a5395dda972dbf13c2266d9
G-K nanochaos <56146097+nanochaos@users.noreply.github.com>
2023-12-26 14:28:57 -0800

    CSS Compression

--- html_minifier.py
+++ html_minifier.py
@@ -1,6 +1,6 @@
 from minify_html import minify_html as mh
 import os
-directory = os.path.join(os.path.dirname(__file__),'KeyClub_Flask_Setup/website/templates')
+directory = os.path.join(os.path.dirname(__file__),'KeyClub_Flask_Setup/website/static/css')
 # iterate over files in
 # that directory
 for filename in os.listdir(directory):
@@ -8,9 +8,14 @@ for filename in os.listdir(directory):
     # checking if it is a file
     if os.path.isfile(f):
         print(f)
-
-    with open(filename,"r+") as file:
-        text = file.read()
-        processed_html = mh.minify(text)
-        print(processed_html)
-        #file.write(processed_html)
+    try:
+        processed_html = ""
+        with open(f,"r") as file:
+            text = file.read()
+            processed_html = mh.minify(text)
+            print(processed_html)
+            #file.write(processed_html)
+        with open(f,"w") as file:
+            file.write(processed_html)
+    except Exception as e:
+        print(e)
